8572eb6cbb740ad313f41bffb289139818159c58
Tessa <tess.starkweather@gmail.com>
2022-10-16 20:19:17 -0400

    color of time block changes with time change

--- Assets/script.js
+++ Assets/script.js
@@ -4,4 +4,16 @@ $(document).ready(function() {
     displayDate.append(currentMoment);
     var currentHour = moment().format("HH");
     console.log(currentHour);
+
+    $(".time-div").each(function() {
+        var timeDiv = $(this).attr("id").split("-")[1];
+        console.log(timeDiv);
+        if (currentHour == timeDiv ) {
+            $(this).addClass("present");
+        } else if(currentHour < timeDiv) {
+            $(this).addClass("future");
+        } else if(currentHour > timeDiv) {
+            $(this).addClass("past");
+        }
+    })
 });
